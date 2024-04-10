86a87fc7677548a447b0d32dc42b8a9372186b3f
NaitikHirani07 <naitikhirani58@gmail.com>
2024-04-10 13:59:12 +0530

    weather second commit

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ async function getData(apiKey,cityName){
     return await data.json();
 }
 
-btnClick.addEventListener('click', async () => {
-    const cityName = document.getElementById('cityName').value;
-    let data = await getData(apiKey,cityName);
-    let locationImg = document.getElementById('locationImgTwo');
-    let locationTemp = document.getElementById('locationTempTwo');
-    let locationName = document.getElementById('locationName');
+if(data.ok){
+    btnClick.addEventListener('click', async () => {
+        const cityName = document.getElementById('cityName').value;
+        let data = await getData(apiKey,cityName);
+        let locationImg = document.getElementById('locationImgTwo');
+        let locationTemp = document.getElementById('locationTempTwo');
+        let locationName = document.getElementById('locationName');
 
-    locationImg.src = data.current.condition.icon;
-    locationTemp.innerHTML = `${data.current.temp_c}&deg`;
-    locationName.innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
-});
+        locationImg.src = data.current.condition.icon;
+        locationTemp.innerHTML = `${data.current.temp_c}&deg`;
+        locationName.innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+    });
+}
